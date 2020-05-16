2a0d67ca0ac8a6773527a7f84b6b7f0caca230ef
treuther <tracyreuther@gmail.com>
2020-05-16 07:55:50 -0500

    setting up fetch staging for MemeGenerator

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 class MemeGenerator extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: ""
+    };
+  }
+
+  componentDidMount() {
+    //fetchData goes here
+  }
+
   render() {
     return (
       <div>
