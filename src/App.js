8ced3e1f7010b3683dc2fd0af7d405d7d0f5c385
treuther <tracyreuther@gmail.com>
2020-05-16 07:45:55 -0500

    render Header and MemeGenerator via App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles.css";
 
+import Header from "./components/Header";
+import MemeGenerator from "./components/MemeGenerator";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Header />
+      <MemeGenerator />
     </div>
   );
 }
