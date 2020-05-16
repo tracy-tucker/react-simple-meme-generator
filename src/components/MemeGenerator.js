ada48bd4e49479e92a5b79d280d62873ccd7c8ea
treuther <tracyreuther@gmail.com>
2020-05-16 08:29:41 -0500

    MemeGenerator - handleChange method added for inputs

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -20,6 +20,13 @@ class MemeGenerator extends React.Component {
       });
   }
 
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
       <div>
@@ -28,13 +35,21 @@ class MemeGenerator extends React.Component {
             name="topText"
             value={this.state.topText}
             placeholder="Top Text"
+            onChange={this.handleChange}
           />
           <input
             name="bottomText"
             value={this.state.bottomText}
             placeholder="Bottom Text"
+            onChange={this.handleChange}
           />
+          <button>Gen</button>
         </form>
+        <div className="meme">
+          <img src={this.state.randomImg} alt="#" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
       </div>
     );
   }
