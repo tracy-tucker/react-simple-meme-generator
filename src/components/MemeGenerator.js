86ebe6bb5666b96852642af8082874e69778a5a8
treuther <tracyreuther@gmail.com>
2020-05-16 07:53:36 -0500

    testing MemeGenerator connection with App

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MemeGenerator extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>testing</p>
+      </div>
+    );
+  }
+}
+
+export default MemeGenerator;
